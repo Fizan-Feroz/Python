d261badf53e5f8bfba3036bf158082d60bffc752
Fizan-Feroz <fizanfaznaferoz2@gmail.com>
2024-01-17 20:43:34 +0530

    bfb

--- rem a2.py
+++ rem a2.py
@@ -2,5 +2,8 @@ file1=open("fizan1.txt",'r')
 file2=open("fizan2.txt",'w')
 for l in file1:
     if "a" not in l:
-        print("a")
+        
         file2.write(l)
+file1.close()
+file2.close()
+print("Removed all lines with 'a'")
